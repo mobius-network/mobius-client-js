9afea001041f365c8c321973a0458bafb0faa4d3
Emil Kashkevich <emil.kashkevich@gmail.com>
2018-05-25 03:35:26 +0200

    Add basic specs for Client

---
+++ test/client.test.js
@@ -0,0 +1,22 @@
+import { Network, Networks } from "stellar-sdk";
+import Client from "../src/client";
+
+describe("Client", () => {
+  describe("should respect global Network config...", () => {
+    test("...on test network", () => {
+      Network.useTestNetwork();
+
+      const clientInstance = new Client();
+
+      expect(clientInstance.network).toEqual(Networks.TESTNET);
+    });
+
+    test("...on public network", () => {
+      Network.usePublicNetwork();
+
+      const clientInstance = new Client();
+
+      expect(clientInstance.network).toEqual(Networks.PUBLIC);
+    });
+  });
+});
