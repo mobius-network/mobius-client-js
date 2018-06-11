e5fd9e31859a8426ba6b35f7afdccde73c4cbbe4
Emil Kashkevich <emil.kashkevich@gmail.com>
2018-06-11 19:52:10 +0200

    Add better documentation for utils/deprecate module

--- src/utils/deprecate.js
+++ src/utils/deprecate.js
@@ -1,8 +1,14 @@
-// Mark that a method should not be used.
-// Returns a modified function which warns once by default.
-// If --no-deprecation is set, then it is a no-op.
+/**
+ * Mark that a method should not be used.
+ * Returns a modified function which warns once by default.
+ * If `--no-deprecation` flag is set , then it is a no-op.
+ * (see for details https://nodejs.org/docs/latest-v8.x/api/cli.html#cli_no_deprecation)
+ *
+ * @param {function} fn - function or method to deprecate
+ * @param {string} msg - deprecation message
+ * @returns {function}
+ */
 export default function deprecate(fn, msg) {
-  // Allow for deprecating things in the process of starting up.
   if (global.process === undefined) {
     return function dep(...args) {
       return exports.deprecate(fn, msg).apply(this, ...args);
@@ -14,6 +20,7 @@ export default function deprecate(fn, msg) {
   }
 
   let warned = false;
+
   function deprecated(...args) {
     if (!warned) {
       if (process.throwDeprecation) {
