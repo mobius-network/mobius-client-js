cbc1454a922aebd270e9e57f605bda7a45d92f42
Sergey Nebolsin <sergey@nebols.in>
2018-07-06 21:02:46 +0300

    Improve syntax, add docs for JWT#decode

--- src/auth/jwt.js
+++ src/auth/jwt.js
@@ -2,7 +2,10 @@ import jwt from "jsonwebtoken";
 
 const alg = "HS512";
 
-/** Generates JWT token based on valid token transaction signed by both parties and decodes JWT token into hash. */
+/**
+ * Generates JWT token based on valid token transaction signed by both parties
+ * and decodes JWT token into hash.
+ */
 export default class JWT {
   /**
    * @param {string} secret - JWT secret
@@ -12,24 +15,28 @@ export default class JWT {
   }
 
   /**
-   * Returns JWT token.
-   * @param {Auth.Token} token Token object
+   * Encode Mobius auth token as a JSON Web Token
+   * @param {Auth.Token} token - Token object
+   * @param {Object} [options={}] - Additional claims for JWT token
    * @returns {string} JWT token
    */
   encode(token, options = {}) {
-    const payload = Object.assign(
-      {
-        jti: token.hash("hex"),
-        sub: token.address,
-        iat: parseInt(token.timeBounds.minTime, 10),
-        exp: parseInt(token.timeBounds.maxTime, 10)
-      },
-      options
-    );
+    const payload = {
+      jti: token.hash("hex"),
+      sub: token.address,
+      iat: parseInt(token.timeBounds.minTime, 10),
+      exp: parseInt(token.timeBounds.maxTime, 10),
+      ...options
+    };
 
     return jwt.sign(payload, this._secret, { algorithm: alg });
   }
 
+  /**
+   * Decode and verify a JSON Web Token.
+   * @param {string} payload - JWT token
+   * @returns {Object} Decoded JWT claims
+   */
   decode(payload) {
     return jwt.verify(payload, this._secret);
   }
